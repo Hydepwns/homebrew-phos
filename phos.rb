1e6f32e787b0e0581b7d957abeef2c4316ea1a85
Hydepwns <drew@axol.io>
2025-12-22 03:59:58 +0100

    phos 0.4.8

--- phos.rb
+++ phos.rb
@@ -5,8 +5,8 @@
 class Phos < Formula
   desc "Universal log colorizer with built-in support for 98 programs"
   homepage "https://github.com/Hydepwns/phos"
-  url "https://github.com/Hydepwns/phos/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "febc203f67053dc0b2acd436a198c9cf39e3770966c8fa080e7cab068f58c6a9"
+  url "https://github.com/Hydepwns/phos/archive/refs/tags/v0.4.8.tar.gz"
+  sha256 "136618e70443563810c6538b58543fb579bf228c139b9b8b784374804004ae2e"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/Hydepwns/phos.git", branch: "main"
 
